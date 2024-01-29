2edf0e267884525bf5c49e28dd5ebf34f6f957ca
dani <danifromecuador@gmail.com>
2024-01-29 17:13:53 -0500

    Create a navigation bar with partials

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,20 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.all.sort_by(&:created_at).reverse
   end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(params[:message])
+
+    if @message.save
+      redirect_to messages_path, notice: 'Message was successfully created.'
+    else
+      render action: 'new', alert: 'Message was not created.', status: :unprocessable_entity
+    end
+  end
+
+  
 end
